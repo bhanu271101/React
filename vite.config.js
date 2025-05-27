08faa99e319cf9c9bb3f00a1261466d21f7ccba6
Bhanuprakash Chinta <chintabhanuprakashreddy@gmail.com>
2025-05-27 12:19:22 +0530

    .env edit

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     historyApiFallback: true,
-    allowedHosts: ['7804-2409-40f2-204c-9c76-142-96d3-99da-c7a2.ngrok-free.app'],
-    // Optional: For HMR to work over ngrok, add the following if you use HTTPS
+    allowedHosts: ['react-2x8o.onrender.com'],
     hmr: {
       clientPort: 443, // or 80 if you use HTTP tunnel
     },
